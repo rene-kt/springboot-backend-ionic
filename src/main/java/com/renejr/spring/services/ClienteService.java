cb9d377e0a6429bda1263fd83cc3afb9a6812df2
Renê <renejr.arraes286@gmail.com>
2020-04-19 16:28:11 -0300

    Consertando erro ao cadastrar cidade ao inserir novo cliente

--- src/main/java/com/renejr/spring/services/ClienteService.java
+++ src/main/java/com/renejr/spring/services/ClienteService.java
@@ -125,12 +125,14 @@ public class ClienteService {
     public Cliente fromDTO(ClienteNewDto objDto) {
 
         Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()), encode.encode(objDto.getSenha()));
-        Cidade cid = new Cidade(objDto.getCidade_id(), null, null);
 
+        Cidade cid = new Cidade(objDto.getCidade_id(), null, null);
+        
+        
         Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
         cli.getEnderecos().add(end);
         cli.getTelefones().add(objDto.getTelefone1());
-        
+
         if (objDto.getTelefone2() != null) {
             cli.getTelefones().add(objDto.getTelefone2());
         }
